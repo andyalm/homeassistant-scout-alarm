6b795a560ab7ae7c015409d2bd2e539bfd8114ac
Jake Colman <jake.colman@gmail.com>
2021-02-21 18:30:04 -0500

    Remove T/H prefix from device name

--- custom_components/scout_alarm/sensor.py
+++ custom_components/scout_alarm/sensor.py
@@ -67,8 +67,7 @@ class ScoutSensor(Entity):
     @property
     def name(self):
         """Return the device name, including the type as a prefix"""
-        """return self._device['name']"""
-        return SENSOR_TYPES.get(self._data_key)[3] + " " + self._device['name']
+        return self._device['name']
 
     @property
     def available(self) -> bool:
